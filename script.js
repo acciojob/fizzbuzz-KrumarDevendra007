eef12cb358a0507a1a2ee8bb35b9615437c3edf9
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-05-22 12:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function printFizzBuzz(num){
+
+	 if(num % 3 === 0){
+        alert(num, "\nFizzBuzz")
+    }
+    else if(num % 5 === 0){
+        alert(num, "\nFizzBuzz")
+    }
+    else{
+        alert(num, "\nFizzBuzz")
+    }
+}
